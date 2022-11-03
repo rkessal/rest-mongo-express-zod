8cffa1995996330420d8e68d7d3899c6b11e70c2
rkessal <kessal.rayhan25@gmail.com>
2022-11-03 17:53:36 +0100

    create server

---
+++ src/utils/logger.ts
@@ -0,0 +1,20 @@
+import logger from "pino";
+import dayjs from "dayjs";
+import pino from "pino";
+
+const transport = pino.transport({
+  target: "pino-pretty",
+  options: { colorize: true },
+});
+
+const log = pino(
+  {
+    base: {
+      pid: false,
+    },
+    timestamp: () => `,"time":"${dayjs().format()}"`,
+  },
+  transport
+);
+
+export default log;
