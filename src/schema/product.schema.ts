c37ae21378445439e35b68ddfbf2829f5cca37e9
rkessal <kessal.rayhan25@gmail.com>
2022-11-03 17:56:08 +0100

    product

---
+++ src/schema/product.schema.ts
@@ -0,0 +1,47 @@
+import { object, string, number, TypeOf } from "zod";
+const payload = {
+  body: object({
+    title: string({
+      required_error: "Title is required",
+    }),
+    description: string({
+      required_error: "Description is required",
+    }).min(120, "Description shoud be atleast 120 characters long"),
+    price: number({
+      required_error: "Number is required",
+    }),
+    image: string({
+      required_error: "Image is required",
+    }),
+  }),
+};
+
+const params = {
+  params: object({
+    productId: string({
+      required_error: "productId is required",
+    }),
+  }),
+};
+
+export const createProductSchema = object({
+  ...payload,
+});
+
+export const updateProductSchema = object({
+  ...params,
+  ...payload,
+});
+
+export const deleteProductSchema = object({
+  ...params,
+});
+
+export const getProductSchema = object({
+  ...params,
+});
+
+export type createProductInput = TypeOf<typeof createProductSchema>;
+export type updateProductInput = TypeOf<typeof updateProductSchema>;
+export type readProductInput = TypeOf<typeof getProductSchema>;
+export type deleteProductInput = TypeOf<typeof deleteProductSchema>;
